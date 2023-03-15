741f4b9ae04eb3b919e744f8e949d7fa04a48913
Darshantada10 <113424552+Darshantada10@users.noreply.github.com>
2023-03-15 15:30:14 +0530

    Add files via upload

---
+++ Module-7(Routes)/routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+Route::get('/',[HomeController::class,'index']);
+Route::get('/home',[HomeController::class,'index']);
+Route::get('/menu',[HomeController::class,'menu']);
+Route::get('/services',[HomeController::class,'services']);
+Route::get('/blog',[HomeController::class,'blog']);
+Route::get('/about',[HomeController::class,'about']);
+Route::get('/contact',[HomeController::class,'contact']);
